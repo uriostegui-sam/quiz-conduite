ab91ef2abe2f461cd6b900ee39677e7e2fcdd510
uriostegui-sam <samyy.uv@gmail.com>
2025-08-09 20:58:42 +0200

    feat(api): support custom serialization for uploads

---
+++ backend/src/Serializer/QuestionNormalizer.php
@@ -0,0 +1,48 @@
+<?php
+// api/src/Serializer/QuestionNormalizer.php
+
+namespace App\Serializer;
+
+use App\Entity\Question;
+use Vich\UploaderBundle\Storage\StorageInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class QuestionNormalizer implements NormalizerInterface
+{
+
+  private const ALREADY_CALLED = 'QUESTION_MEDIA_NORMALIZER_ALREADY_CALLED';
+
+  public function __construct(
+    #[Autowire(service: 'api_platform.jsonld.normalizer.item')]
+    private readonly NormalizerInterface $normalizer,
+    private readonly StorageInterface $storage
+  ) {
+  }
+
+  public function normalize($object, ?string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
+  {
+    $context[self::ALREADY_CALLED] = true;
+
+    $object->contentUrl = $this->storage->resolveUri($object, 'mediaFile');
+
+    return $this->normalizer->normalize($object, $format, $context);
+  }
+
+  public function supportsNormalization($data, ?string $format = null, array $context = []): bool
+  {
+
+    if (isset($context[self::ALREADY_CALLED])) {
+      return false;
+    }
+
+    return $data instanceof Question;
+  }
+
+  public function getSupportedTypes(?string $format): array
+  {
+    return [
+      Question::class => true,
+    ];
+  }
+}
