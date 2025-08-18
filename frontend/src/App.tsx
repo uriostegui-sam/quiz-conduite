978046d46a87592f394d57cc94bbc40e674ce191
uriostegui-sam <samyy.uv@gmail.com>
2025-08-18 18:07:55 +0200

    feat(visitor): install react-query and axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import Quiz from './Components/Quiz/Quiz'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const App = () => {
+  const client = new QueryClient();
   return (
+    <QueryClientProvider client={client}>
       <Quiz />
+    </QueryClientProvider>
   )
 }
 
