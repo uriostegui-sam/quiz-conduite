2f04367f84627e9fd1c1c541ca894888dade2ad0
uriostegui-sam <samyy.uv@gmail.com>
2025-08-19 13:14:29 +0200

    feat(quiz): create query to get the data to create the quiz

--- frontend/src/Components/Quiz/Quiz.tsx
+++ frontend/src/Components/Quiz/Quiz.tsx
@@ -1,23 +1,36 @@
-import Answer from "./AnswerComponent";
+import './Quiz.css'
+import axios from "axios";
+import Card from "./Card";
 import Button from "./ButtonComponent";
-import Title from "./TitleComponent";
+import { useQuery } from "@tanstack/react-query";
+import type { Question } from "../../Interfaces/Question";
 
 const Quiz = () => {
+  const { data, isLoading, error } = useQuery<Question[], Error>({
+    queryKey: ["questions"],
+    queryFn: async () => {
+      const response = await axios.get<Question[]>(
+        import.meta.env.VITE_API_URL + "/questions/random?count=1&theme=QSER"
+      );
+      return response.data;
+    },
+  });
+
+  const questionCards = data?.map((question) => (
+    <Card key={question.id} card={question} />
+  ));
+
   return (
-    <div className="container flex flex-col items-center justify-center h-screen">
+    <div className="container flex flex-col gap-5 justify-center items-center">
       <h1>Quiz conduite</h1>
       <hr />
-      <div className="1 flex flex-col items-center justify-center gap-5">
-        <Title />
-        <ul className="flex flex-col gap-3">
-          <Answer />
-          <Answer />
-          <Answer />
-          <Answer />
-        </ul>
-        <Button />
-        <div className="question-number">1 of 5 questions</div>
+      <div>
+        {isLoading && <p>Loading questions...</p>}
+        {error && <p className="text-red-500">Error loading questions</p>}
+        {data && questionCards}
       </div>
+        <Button />
+        <div className="question-number">1 of {data?.length} questions</div>
     </div>
   );
 };
