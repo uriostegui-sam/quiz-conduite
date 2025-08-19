d28851534fe4d5a819d81d383ddfb7a1ea57c4f8
uriostegui-sam <samyy.uv@gmail.com>
2025-08-19 13:09:11 +0200

    feat(visitor-answer): add the logic to check answers

--- frontend/src/Components/Quiz/AnswerComponent.tsx
+++ frontend/src/Components/Quiz/AnswerComponent.tsx
@@ -1,10 +1,38 @@
-const Answer = () => {
-  return (
-    <li className="border-1 rounded-md border-cyan-700 flex items justify-center py-2 px-3">
-    - Rouge : Une anomalie de fonctionnement ou un danger.\n- Orange :
-    un élément important.
-    </li>
-  )
-}
+const AnswerComponent = (props: {
+  answer: string;
+  incorrectAnswers: Array<string>;
+  correctAnswer: string;
+  setChosenAnswer: (answer: string) => void;
+  chosenAnswer: string | null;
+}) => {
+  const selectAnswer = (e: React.MouseEvent<HTMLLIElement>) => {
+    if (props.chosenAnswer) return;
+    const input = e.target as HTMLElement;
+    props.setChosenAnswer(input.textContent || "");
+  };
 
-export default Answer
+  const checkAnswer = () => {
+    if (props.answer === props.chosenAnswer) {
+      if (props.chosenAnswer === props.correctAnswer)
+        return "bg-green-100 border-green-100";
+      else return "bg-red-100 border-red-100";
+    }
+
+    if (props.chosenAnswer !== null) {
+      if (props.correctAnswer === props.answer)
+        return "bg-green-100 border-cyan-700";
+    }
+    return "bg-white border-cyan-700 hover:bg-[#00759516] hover:border-[#00759516]";
+  };
+
+  return (
+    <li
+      onClick={selectAnswer}
+      className={`border-1 rounded-md flex items justify-center py-2 px-3 ${checkAnswer()}`}
+    >
+      {props.answer}
+    </li>
+  );
+};
+
+export default AnswerComponent;
