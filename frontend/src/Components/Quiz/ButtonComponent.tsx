615610b2bcdb174017d9d26295ab29df2df1cc3f
uriostegui-sam <samyy.uv@gmail.com>
2025-08-21 17:26:07 +0200

    feat(button): modify button to use it to restart the game

--- frontend/src/Components/Quiz/ButtonComponent.tsx
+++ frontend/src/Components/Quiz/ButtonComponent.tsx
@@ -1,29 +1,40 @@
 import ArrowRight from "../../assets/icon/arrow-right.svg";
+import Repeat from "../../assets/icon/repeat.svg";
 
 const Button = (props: {
-  index: number;
-  setIndex: (index: number) => void;
-  chosenAnswer: string | null;
+  index?: number;
+  setIndex?: (index: number) => void;
+  chosenAnswer?: string | null;
+  inQuiz: boolean;
 }) => {
   const next = () => {
     if (props.chosenAnswer !== null) {
-      props.setIndex(props.index + 1);
-      (document.getElementById("nextButton") as HTMLButtonElement).disabled =
-        false;
+      if(props.setIndex && props.index) props.setIndex(props.index + 1);
     }
   };
 
+  const restart = () => {
+    return;
+  };
+
+  const isNotClicked = props.chosenAnswer === null;
   return (
     <button
-      id="nextButton"
-      onClick={next}
-      className="relative h-12 overflow-hidden rounded bg-cyan-700 px-5 py-2.5 text-white transition-all duration-300 hover:bg-cyan-700 hover:ring-2 hover:ring-cyan-700 hover:ring-offset-2"
-      disabled={props.chosenAnswer === null}
+      onClick={props.inQuiz ? next : restart}
+      className={`relative h-12 overflow-hidden rounded px-5 py-2.5 text-white transition-all duration-300 bg-cyan-700 hover:bg-cyan-700 hover:ring-2 hover:ring-cyan-700 hover:ring-offset-2 disabled:bg-gray-200 disabled:border-gray-200`}
+      disabled={props.inQuiz ? isNotClicked : false}
     >
-      <span className="relative flex items-center gap-3">
-        <p>Next question</p>
-        <img src={ArrowRight} className="w-5 h-5" />
-      </span>
+      {props.inQuiz ? (
+        <span className="relative flex items-center gap-3">
+          <p>Next question</p>
+          <img src={ArrowRight} className="w-5 h-5" />
+        </span>
+      ) : (
+        <span className="relative flex items-center gap-3">
+          <p>Restart game</p>
+          <img src={Repeat} className="w-5 h-5" />
+        </span>
+      )}
     </button>
   );
 };
