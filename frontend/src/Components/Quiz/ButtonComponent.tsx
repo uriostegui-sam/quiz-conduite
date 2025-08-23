fbd3dfef4c1ba39542a74662fc478c673568b6b2
uriostegui-sam <samyy.uv@gmail.com>
2025-08-23 15:01:00 +0200

    feat(button): add start and restart button functionality with new icons

--- frontend/src/Components/Quiz/ButtonComponent.tsx
+++ frontend/src/Components/Quiz/ButtonComponent.tsx
@@ -1,11 +1,19 @@
 import ArrowRight from "../../assets/icon/arrow-right.svg";
 import Repeat from "../../assets/icon/repeat.svg";
+import Play from "../../assets/icon/play.svg";
 
 const Button = (props: {
   index?: number;
   setIndex?: (index: number) => void;
   chosenAnswer?: string | null;
   inQuiz: boolean;
+  startButton?: boolean;
+  start?: boolean;
+  setStart?: (start: boolean) => void;
+  reStart?: boolean;
+  setReStart?: (reStart: boolean) => void;
+  playing?: boolean;
+  setPlaying?: (playing: boolean) => void;
 }) => {
   const next = () => {
     if (props.chosenAnswer !== null) {
@@ -13,23 +21,36 @@ const Button = (props: {
     }
   };
 
+  const start = () => {
+    if(props.setStart) props.setStart(true)
+  };
+
   const restart = () => {
-    return;
+    if(props.setReStart) props.setReStart(true)
+    if(props.setStart) props.setStart(true)
   };
 
   const isNotClicked = props.chosenAnswer === null;
+
   return (
     <button
-      onClick={props.inQuiz ? next : restart}
+      onClick={props.inQuiz ? next : (props.startButton ? start : restart)}
       className={`relative h-12 overflow-hidden rounded px-5 py-2.5 text-white transition-all duration-300 bg-cyan-700 hover:bg-cyan-700 hover:ring-2 hover:ring-cyan-700 hover:ring-offset-2 disabled:bg-gray-200 disabled:border-gray-200`}
       disabled={props.inQuiz ? isNotClicked : false}
     >
-      {props.inQuiz ? (
+      {props.inQuiz && (
         <span className="relative flex items-center gap-3">
           <p>Next question</p>
           <img src={ArrowRight} className="w-5 h-5" />
         </span>
-      ) : (
+      )}
+      {props.startButton && (
+        <span className="relative flex items-center gap-3">
+          <p>Start the game</p>
+          <img src={Play} className="w-5 h-5" />
+        </span>
+      )}
+      {!props.inQuiz && !props.startButton && (
         <span className="relative flex items-center gap-3">
           <p>Restart game</p>
           <img src={Repeat} className="w-5 h-5" />
