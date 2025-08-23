519bd87cb7e02d23011b1fad16d7d74daf67e2ee
uriostegui-sam <samyy.uv@gmail.com>
2025-08-23 15:00:00 +0200

    feat(result): display category and add restart support

--- frontend/src/Components/Result/ResultQuestionsComponent.tsx
+++ frontend/src/Components/Result/ResultQuestionsComponent.tsx
@@ -3,18 +3,35 @@ import Button from "../Quiz/ButtonComponent";
 const ResultQuestionsComponent = (props: {
   score: number;
   totalQuestions: number;
+  category: string;
+  categoryOptions: Record<string, string>;
+  start?: boolean;
+  setStart?: (start: boolean) => void;
+  reStart?: boolean;
+  setReStart?: (reStart: boolean) => void;
+  index?: number;
+  setIndex?: (index: number) => void;
 }) => {
-
   return (
     <div className="container flex flex-col gap-10 justify-center items-center">
       <h2 className="text-4xl">
-        {/* {props.category} */}
-        titre
+        Categorie :{" "}
+        <span className="font-semibold">
+          {props.categoryOptions[props.category]}
+        </span>
       </h2>
       <h3 className="text-3xl">
         You scored {props.score}/{props.totalQuestions} correct answers
       </h3>
-      <Button inQuiz={false} />
+      <Button
+        inQuiz={false}
+        start={props.start}
+        setStart={props.setStart}
+        reStart={props.reStart}
+        setReStart={props.setReStart}
+        index={props.index}
+        setIndex={props.setIndex}        
+      />
     </div>
   );
 };
