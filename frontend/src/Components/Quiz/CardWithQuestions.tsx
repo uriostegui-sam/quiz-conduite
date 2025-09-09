c28c74c8ed966038a2bd0369b9acff211a8a907c
uriostegui-sam <samyy.uv@gmail.com>
2025-09-09 21:38:06 +0200

    feat: add visual feedback for answered cards with color coding and icons

--- frontend/src/Components/Quiz/CardWithQuestions.tsx
+++ frontend/src/Components/Quiz/CardWithQuestions.tsx
@@ -3,6 +3,8 @@ import "./CardWithQuestions.css";
 import QuestionComponent from "./QuestionComponent";
 import type { Card } from "../../Interfaces/Card";
 import type { Question } from "../../Interfaces/Question";
+import Check from "../../assets/icon/circle-check.svg";
+import Xmark from "../../assets/icon/circle-xmark.svg";
 
 const CardWithQuestions = (props: {
   index: number;
@@ -19,6 +21,8 @@ const CardWithQuestions = (props: {
   setAnsweredQuestions: React.Dispatch<React.SetStateAction<number[]>>;
 }) => {
   const [isCard, setIsCard] = useState<boolean>(true);
+  const [isCorrect, setIsCorrect] = useState<boolean | null | undefined>(null)
+  const [questionResults, setQuestionResults] = useState<Record<number, 'correct'|'incorrect'|null>>({});
 
   const activeQuestion = props.cardData.questions.find(
     (question) => question.id === props.activeQuestion
@@ -31,14 +35,26 @@ const CardWithQuestions = (props: {
     >
       {props.cardData.questions.map((question: Question) => {
         const isAnswered = props.answeredQuestions.includes(question.id);
+        const cardColor = (isCorrect: string | null) => {
+          if(isCorrect === 'correct') {
+            return 'bg-green-100'
+          } else if (isCorrect === 'incorrect') {
+            return 'bg-red-100'
+          } else {
+            return 'bg-neutral-100'
+          }
+        };
 
         return (
           <div
             key={question.id}
-            className={`card ${isAnswered ? "answered" : ""}`}
+            className={`card ${isAnswered ? "answered" : ""} ${cardColor(questionResults[question.id])}`}
             onClick={() => props.flipTheCard(question.id)}
           >
-            <div className="front">{question.category}</div>
+            <div className={`front`}>
+              <div className="category">{question.category}</div>
+              <div>{isAnswered ? <img src={questionResults[question.id] === 'correct' ? Check : Xmark} className="w-5 h-5" /> : ""}</div>
+            </div>
           </div>
         );
       })}
@@ -66,7 +82,15 @@ const CardWithQuestions = (props: {
                     activeQuestion.id,
                   ]); // mark answered
                   props.setActiveQuestion(null); // close
+                  setQuestionResults((prev) => ({
+                    ...prev,
+                    [activeQuestion.id]: isCorrect ? 'correct' : 'incorrect'
+                  }))
                 }}
+                questionResults={questionResults}
+                setQuestionResults={setQuestionResults}
+                isCorrect={isCorrect}
+                setIsCorrect={setIsCorrect}
               />
             )}
           </div>
