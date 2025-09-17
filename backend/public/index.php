55fa365dc9300656ee419ae85762560310cf8ef8
uriostegui-sam <samyy.uv@gmail.com>
2025-09-17 21:45:15 +0200

    fix(index): force Symfony to stop reading .env

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,6 +2,16 @@
 
 use App\Kernel;
 
+// Force Symfony not to load .env
+putenv('APP_RUNTIME_DONTLOADENV=1');
+$_SERVER['APP_RUNTIME_DONTLOADENV'] = '1';
+$_ENV['APP_RUNTIME_DONTLOADENV'] = '1';
+
+// also force APP_ENV
+putenv('APP_ENV=prod');
+$_SERVER['APP_ENV'] = 'prod';
+$_ENV['APP_ENV'] = 'prod';
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
